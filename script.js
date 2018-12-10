1e19e5676b65b4918aa9750e3cac89145e9caaa2
David Pedrick <36094521+moxypedrick@users.noreply.github.com>
2018-12-09 19:22:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var bounds = [
 mapboxgl.accessToken = 'pk.eyJ1IjoibW94eXBlZCIsImEiOiJjaWgydGpwdmYweHJydnFtMzZzOXpmNjg3In0.5TXWYv0Z7nsOZHneIQOhxg';
 
 var map = new mapboxgl.Map({
-  
     container: 'map',
     style: 'mapbox://styles/moxyped/cjn6fq1br115i2snaywee8zce',
     center: [-84.380378, 33.767279],
@@ -17,8 +16,8 @@ var map = new mapboxgl.Map({
     customAttribution: ['<a href=https://atlantaregional.org/ target=_blank>Atlanta Regional Comission</a>','<a href=https://www.epa.gov/eco-research/ecoregions target=_blank>EPA Eco-Regions</a>','<a href=https://hdsc.nws.noaa.gov/hdsc/pfds/pfds_map_cont.html?bkmrk=ga target=_blank>NOAA - Storm Data</a>', '<a href=https://www.epa.gov/watersense/water-budget-data-finder target=_blank>EPA - ETP</a>']
 });
 
-map.on('load', function() {
 
+map.on('load', function() {
   map.addSource('soGeorgia', {
     'type' : 'geojson',
     'data' : 'https://rawgit.com/moxypedrick/ECC-Map/master/S_Georgia.geojson' 
@@ -48,12 +47,12 @@ map.on('load', function() {
       'fill-outline-color' : '#000000'    
     }
   }, 'soGeorgiaLayer')
-  
-  map.addSource('isochrone', {
-    'type' : 'geojson',
-    'data' : 'https://api.mapbox.com/isochrone/v1/mapbox/walking/-84.380378,33.767279?contours_minutes=5,10&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=pk.eyJ1IjoibW94eXBlZCIsImEiOiJjaWgydGpwdmYweHJydnFtMzZzOXpmNjg3In0.5TXWYv0Z7nsOZHneIQOhxg' 
-  })
 
+   map.addSource('isochrone', {
+    'type' : 'geojson',
+    'data' : 'https://api.mapbox.com/isochrone/v1/mapbox/walking/-84.380378,33.767279?contours_minutes=2,5,7,10&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=pk.eyJ1IjoibW94eXBlZCIsImEiOiJjaWgydGpwdmYweHJydnFtMzZzOXpmNjg3In0.5TXWYv0Z7nsOZHneIQOhxg' 
+  })
+ 
   map.addLayer({'id' : 'isochroneLayer',
     'type' : 'fill',
     'source': 'isochrone', 
@@ -63,20 +62,26 @@ map.on('load', function() {
       'fill-outline-color' : '#000000' 
     }
   }, 'noGeorgiaLayer')
+
 });
 
-map.on('mousemove','soGeorgiaLayer', function(e){
+map.on('click','soGeorgiaLayer', function(e){
   var features = map.queryRenderedFeatures(e.point);
+  console.log(e);
   updateArea(features);
 })
-map.on('mousemove','noGeorgiaLayer', function(e){
-  var features = map.queryRenderedFeatures(e.point);
+
+
+
+map.on('click','noGeorgiaLayer', function(e){
+   var features = map.queryRenderedFeatures(e.point);
   updateArea(features);
+  //console.log(features);
 })
 
 var draw = new MapboxDraw({
     displayControlsDefault: false,
-    //position : 'top-left,
+
     controls: {
       point: true,
       polygon: true,
@@ -86,8 +91,6 @@ var draw = new MapboxDraw({
 
 function updateArea(e){
   var features = e;
-  //var data = draw.getAll(); 
-  //console.log(features);
   document.getElementById('netET').innerHTML = features[0].properties.Net_ET;
   document.getElementById('oneYear').innerHTML = features[0].properties.Inches_2;
   document.getElementById('ecoRegion').innerHTML = features[0].properties.NA_L3NAME;
@@ -96,8 +99,6 @@ function updateArea(e){
 
 map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
-    //position : 'top-left',
-    //limit results to Georgia
     country: 'us',
     region: 'Georgia',
     bbox:  [-86.010452, 30.537327, -80.771171, 35.210589] ,
@@ -105,10 +106,10 @@ map.addControl(new MapboxGeocoder({
 
 map.on('click', function(e){
   if(event.ctrlKey){
-  var urlIsochrone = 'https://api.mapbox.com/isochrone/v1/mapbox/walking/'+e.lngLat.lng+','+e.lngLat.lat+'?contours_minutes=5,10&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=pk.eyJ1IjoibW94eXBlZCIsImEiOiJjaWgydGpwdmYweHJydnFtMzZzOXpmNjg3In0.5TXWYv0Z7nsOZHneIQOhxg'
-    
-  map.getSource('isochrone').setData(urlIsochrone);
-  }
+    console.log("The CTRL key was pressed!")
+    var urlIsochrone = 'https://api.mapbox.com/isochrone/v1/mapbox/walking/'+e.lngLat.lng+','+e.lngLat.lat+'?contours_minutes=2,3,4,5&contours_colors=6706ce,04e813,4286f4&polygons=true&access_token=pk.eyJ1IjoibW94eXBlZCIsImEiOiJjaWgydGpwdmYweHJydnFtMzZzOXpmNjg3In0.5TXWYv0Z7nsOZHneIQOhxg'
+    map.getSource('isochrone').setData(urlIsochrone);
+  } 
 });
 
 var scale = new mapboxgl.ScaleControl({
@@ -116,10 +117,28 @@ var scale = new mapboxgl.ScaleControl({
     unit: 'imperial'
 });
 map.addControl(scale, 'bottom-right');
-
 scale.setUnit('imperial');
-
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 map.addControl(draw, 'top-right');
+
+function addFields(){
+  var number = draw.getAll().features.length;
+  var json = draw.getAll().features;
+
+  var container = document.getElementById("user-input-container");
+    while (container.hasChildNodes()) {
+      container.removeChild(container.lastChild);
+    }
+    for (i=0;i<number;i++){
+      container.appendChild(document.createTextNode("Feature " + (i+1) + (" ")));
+      var feature_id = "Feature_"+(i+1);
+      var input = document.createElement("input");
+      input.type = "text";
+      input.id = feature_id;
+      input.value = json[i].id;
+      container.appendChild(input);
+      container.appendChild(document.createElement("br"));
+    }
+}
 
 })();
