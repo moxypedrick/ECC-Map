60f9ae7f5c575e9bf4b053a7550a7a7401ea92b8
David Pedrick <36094521+moxypedrick@users.noreply.github.com>
2018-12-10 20:57:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var map = new mapboxgl.Map({
     center: [-84.380378, 33.767279],
     zoom: 4,
     maxBounds: bounds,
-    customAttribution: ['<a href=https://atlantaregional.org/ target=_blank>Atlanta Regional Comission</a>','<a href=https://www.epa.gov/eco-research/ecoregions target=_blank>EPA Eco-Regions</a>','<a href=https://hdsc.nws.noaa.gov/hdsc/pfds/pfds_map_cont.html?bkmrk=ga target=_blank>NOAA - Storm Data</a>', '<a href=https://www.epa.gov/watersense/water-budget-data-finder target=_blank>EPA - ETP</a>']
+    customAttribution: ['<a href=https://atlantaregional.org/ target=_blank>Atlanta Regional Commission</a>','<a href=https://www.epa.gov/eco-research/ecoregions target=_blank>EPA Eco-Regions</a>','<a href=https://hdsc.nws.noaa.gov/hdsc/pfds/pfds_map_cont.html?bkmrk=ga target=_blank>NOAA - Storm Data</a>', '<a href=https://www.epa.gov/watersense/water-budget-data-finder target=_blank>EPA - ETP</a>']
 });
 
 
